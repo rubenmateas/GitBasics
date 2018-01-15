4d0d054255d4f5855258eadd85e2183c9962c112
Ruben Mateas <rubenmaetas1313@outlook.com>
2018-01-14 22:46:30 -0800

    Code With Loops

---
+++ app.js
@@ -0,0 +1,30 @@
+// Fizz
+
+// for (var i = 1; i <= 300; i++) {
+//   if(i%5 === 0){
+//     console.log("Fizz");
+//   }else if(i%7 === 0){
+//     console.log("Fuzz");
+//   }else{
+//     console.log(i);
+//   }
+// }
+
+// FixxFuzz
+
+// for (var i = 1; i <= 300; i++) {
+//   if(i%3 === 0 && i%5 === 0){
+//     console.log("FizzFuzz");
+//   }else{
+//     console.log(i);
+//   }
+// }
+
+// Triangle
+
+// var tri = "#######";
+// var num = tri.length;
+// for (var i = 0; i < num; i++) {
+//   console.log(tri);
+//   tri = tri.slice(0, -1);
+// }
